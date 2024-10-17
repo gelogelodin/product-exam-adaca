7303fd538e58ce3ba476b2dc5b344b3a3f42bcd4
gelogelodin <bantiling.angelo@gmail.com>
2024-10-17 20:17:50 +0800

    Feature: removed mock data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,10 +6,7 @@ import { Product } from './models/product.model';
   providedIn: 'root'
 })
 export class ProductService {
-  private products: Product[] = [
-    { id: 1, name: 'Product 1', description: 'Description 1', price: 100 },
-    { id: 2, name: 'Product 2', description: 'Description 2', price: 200 }
-  ];
+  private products: Product[] = [];
 
   private productsSubject = new BehaviorSubject<Product[]>(this.products);
 
